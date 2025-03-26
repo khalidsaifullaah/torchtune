620469b3a05033e724ed606bf9b2171640a20e9c
Monte Hoover <monte.b.hoover@gmail.com>
2025-03-26 01:39:13 -0400

    Update recipes

--- launch_main.sh
+++ launch_main.sh
@@ -1,12 +1,10 @@
 # launch with:
 # launch launch_main.sh --classical_logfile_names --gpu_type rtxa5000 --mem 30
+# or
+# launch launch_main.sh --classical_logfile_names --gpu_type rtxa6000 --mem 30 --qos vulcan-high_long
 
-tune run lora_finetune_single_device --config recipes/configs/guardian_models/1B_lora_2500.yaml
-tune run lora_finetune_single_device --config recipes/configs/guardian_models/1B_lora_5000.yaml
-tune run lora_finetune_single_device --config recipes/configs/guardian_models/1B_lora_7500.yaml
-tune run lora_finetune_single_device --config recipes/configs/guardian_models/3B_lora_2500.yaml
-tune run lora_finetune_single_device --config recipes/configs/guardian_models/3B_lora_5000.yaml
-tune run lora_finetune_single_device --config recipes/configs/guardian_models/3B_lora_7500.yaml
-tune run lora_finetune_single_device --config recipes/configs/guardian_models/8B_lora_2500.yaml
-tune run lora_finetune_single_device --config recipes/configs/guardian_models/8B_lora_5000.yaml
-tune run lora_finetune_single_device --config recipes/configs/guardian_models/8B_lora_7500.yaml
+tune run lora_finetune_single_device --config recipes/configs/guardian_models/qwen_2_5_05B_lora_multirule.yaml
+tune run lora_finetune_single_device --config recipes/configs/guardian_models/qwen_2_5_1B_lora_multirule.yaml
+tune run lora_finetune_single_device --config recipes/configs/guardian_models/qwen_2_5_3B_lora_multirule.yaml
+tune run lora_finetune_single_device --config recipes/configs/guardian_models/qwen_2_5_7B_lora_multirule.yaml
+tune run lora_finetune_single_device --config recipes/configs/guardian_models/qwen_2_5_14B_lora_multirule.yaml
